631adc67fba669f259256f62b0b1fc403a5df99e
VladislavChobanu <m.akckaunt@yandex.ru>
2024-04-11 16:11:45 +0300

    UserPage

--- client/src/components/pages/UserPage.tsx
+++ client/src/components/pages/UserPage.tsx
@@ -1,13 +1,20 @@
 import { Avatar, Flex, Text, WrapItem } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useAppSelector } from '../../hooks/useReduxHook'
+import PointCard from '../ui/PointCard'
+import { useDispatch } from 'react-redux'
+import { getPointsThunk } from '../../redux/thunkActions/mapThunkAction'
 
 type Props = {}
 
 export default function UserPage({}: Props) {
+  const dispatch = useDispatch();
   const user = useAppSelector((state) => state.auth.user);
-  const points = useAppSelector((state) => state.point.points)
+  const {points} = useAppSelector((state) => state.point)
+
+  useEffect(() => {
+    void dispatch(getPointsThunk());
+  }, [dispatch]);
 
   return (
     <div>
@@ -17,14 +24,10 @@ export default function UserPage({}: Props) {
     <Text>{user.status === 'logged' ? user.username : 'guest'}</Text>
     </WrapItem>
     <Flex>
-      {points.map((point)=> (
-        <Card>
-        <CardBody>
-          <Text>Тема разговора: {point.theme}</Text><br />
-          <Text>Отличительные черты вашего партнёра: {point.cloth}</Text><br />
-        </CardBody>
-      </Card>
-      ))}
+      Ваши разговоры:
+      {points.map((point)=> 
+        <PointCard point={point} key={point.id} user={user}/>
+      )}
     </Flex>
     </div>
   )
