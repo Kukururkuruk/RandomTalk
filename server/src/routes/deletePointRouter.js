111c2b42e67cc393be762606ec16d96eb52a3063
feek00s <ivanov19251@gmail.com>
2024-04-11 16:14:20 +0300

    потенцально рабочий делит без хэйндлера

---
+++ server/src/routes/deletePointRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { Point } = require('../../db/models');
+const verifyAccessToken = require('../middlewares/verifyAccessToken');
+
+const apiDeletePointRouter = express.Router();
+
+apiDeletePointRouter
+  .route('/:id')
+  .delete(verifyAccessToken, async (req, res) => {
+    await Point.destroy({
+      where: { id: req.params.id },
+    });
+    res.sendStatus(200);
+  })
+
+module.exports = apiDeletePointRouter;
