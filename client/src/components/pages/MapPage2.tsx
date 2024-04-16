ed73ecf0d8ff20087e8d4cbace8e796bc1b80431
AlexeyZhaloba <garringo@gmail.com>
2024-04-16 09:26:25 +0300

    blabla

--- client/src/components/pages/MapPage2.tsx
+++ client/src/components/pages/MapPage2.tsx
@@ -2,10 +2,24 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHook';
 import { getPointsThunk } from '../../redux/thunkActions/mapThunkAction';
+import giphy from '../../giphy.gif';
+import giphy1 from '../../giphy (1).gif';
+import giphy2 from '../../giphy (2).gif';
+import giphy3 from '../../giphy (3).gif';
+import giphy4 from '../../giphy (4).gif';
+
+const gifs = [
+  { id: 1, src: giphy },
+  { id: 2, src: giphy1 },
+  { id: 3, src: giphy2 },
+  { id: 4, src: giphy3 },
+  { id: 5, src: giphy4 },
+];
 
 export default function MapPage2({ onCoordinateSelection }): JSX.Element {
   const points = useAppSelector((store) => store.point.points);
   const [mapClickCoords, setMapClickCoords] = useState(null);
+  const [selectedGif, setSelectedGif] = useState(gifs[0]);
 
   const dispatch = useAppDispatch();
 
@@ -28,7 +42,7 @@ export default function MapPage2({ onCoordinateSelection }): JSX.Element {
   return (
     <YMaps query={{ apikey: '2625da4a-fb70-440e-b1f7-b2d072017058' }}>
       <Map
-      style={mapStyle}
+        style={mapStyle}
         defaultState={{
           center: [55.75222, 37.61763],
           zoom: 10,
@@ -50,15 +64,13 @@ export default function MapPage2({ onCoordinateSelection }): JSX.Element {
               balloonContentHeader: `${object.theme}`,
               balloonContentBody: `${object.cloth}`,
             }}
-            // options={{
-            //   iconLayout: 'default#image',
-            //   iconImageHref: myIcon,
-            //   iconImageSize: [30, 42],
-            //   iconImageOffset: [-3, -42],
-            // }}
-            modules={
-              ['geoObject.addon.balloon', 'geoObject.addon.hint']
-          }
+            options={{
+              iconLayout: 'default#image',
+              iconImageHref: giphy,
+              iconImageSize: [30, 42],
+              iconImageOffset: [-3, -42],
+            }}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
           />
         ))}
 
@@ -68,6 +80,17 @@ export default function MapPage2({ onCoordinateSelection }): JSX.Element {
             options={{
               zIndex: 100,
             }}
+            properties={{
+              balloonContentHeader: `asdasdasd`,
+              balloonContentBody: `asdasdasdad`,
+            }}
+            options={{
+              iconLayout: 'default#image',
+              iconImageHref: giphy,
+              iconImageSize: [30, 42],
+              iconImageOffset: [-3, -42],
+            }}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
           />
         )}
       </Map>
