716b66db9b5177cb4637abef395476fadaf0248d
AlexeyZhaloba <garringo@gmail.com>
2024-04-17 17:01:42 +0300

    NewBegin

--- client/src/components/ui/PointCard.tsx
+++ client/src/components/ui/PointCard.tsx
@@ -1,3 +1,67 @@
+// import React, { useState, useEffect } from 'react';
+// import { Button, Card, CardBody, Text } from '@chakra-ui/react';
+// import type { PointType } from '../../types/PointType';
+// import type { UserStateType } from '../../types/authType';
+// import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHook';
+// import { banPointThunk } from '../../redux/thunkActions/addPointThunk';
+// import { updateAgreePointThunk } from '../../redux/thunkActions/updatePointThunk';
+// import getClientThunk from '../../redux/thunkActions/getClientThunk';
+
+// type PointCardProps = {
+//   point: PointType;
+//   user: UserStateType;
+// };
+
+// export default function PointCard({ point, user }: PointCardProps) {
+//   const dispatch = useAppDispatch();
+//   const clientName = useAppSelector((state) => state.client.username);
+//   const [buttonsVisible, setButtonsVisible] = useState(true);
+
+//   const banHandler = () => {
+//     dispatch(banPointThunk({ userId: point.clientId, pointId: point.id }));
+//   };
+
+//   useEffect(() => {
+//     void dispatch(getClientThunk(point.clientId));
+//   }, [dispatch]);
+
+//   const handleApplyButtonClick = () => {
+//     dispatch(updateAgreePointThunk(point.id));
+//     setButtonsVisible(false);
+//   };
+
+//   if (point.userId === (user.status === 'logged' ? user.id : '')) {
+//     return (
+//       <Card border="2px" borderColor={point.status === true ? 'green' : 'red'}>
+//         <CardBody>
+//           <Text>Тема разговора: {point.theme}</Text>
+//           <br />
+//           {point.status === true && (
+//             <>
+//               <Text>Имя вашего собеседника: {clientName}</Text>
+//               <Text>Описание вашего собеседника: {point.reason}</Text>
+//               <br />
+//             </>
+//           )}
+//         </CardBody>
+//         {point.status === true && buttonsVisible && (
+//           <>
+//             <Button bg="green" textColor="white" onClick={handleApplyButtonClick}>
+//               Apply
+//             </Button>
+//             <Button bg="red" textColor="white" onClick={banHandler}>
+//               Reject
+//             </Button>
+//           </>
+//         )}
+//       </Card>
+//     );
+//   }
+//   return null;
+// }
+
+
+
 import React, { useState, useEffect } from 'react';
 import { Button, Card, CardBody, Text } from '@chakra-ui/react';
 import { PointType } from '../../types/PointType';
@@ -6,6 +70,7 @@ import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHook';
 import { updateAgreePointThunk } from '../../redux/thunkActions/updatePointThunk';
 import getClientThunk from '../../redux/thunkActions/getClientThunk';
 import { addHistoryThunk } from '../../redux/thunkActions/historyThunk';
+import { banPointThunk } from '../../redux/thunkActions/addPointThunk';
 
 type PointCardProps = {
   point: PointType;
@@ -27,6 +92,12 @@ export default function PointCard({ point, user }: PointCardProps) {
     setButtonsVisible(false);
   };
 
+  const banHandler = () => {
+    dispatch(banPointThunk({ userId: point.clientId, pointId: point.id }));
+  };
+
+
+
   if (point.userId === (user.status === 'logged' ? user.id : '')) {
     return (
       <Card border="2px" borderColor={point.status === true ? 'green' : 'red'}>
@@ -46,7 +117,9 @@ export default function PointCard({ point, user }: PointCardProps) {
             <Button bg="green" textColor="white" onClick={handleApplyButtonClick}>
               Apply
             </Button>
-            <Button bg="red" textColor="white">Reject</Button>
+            <Button bg="red" textColor="white" onClick={banHandler}>
+              Reject
+            </Button>
           </>
         )}
       </Card>
