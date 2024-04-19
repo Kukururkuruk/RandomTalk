79707c08615f5bbc6cd68fb7bf809e74ea035246
feek00s <ivanov19251@gmail.com>
2024-04-19 14:13:06 +0300

    случайно в дэв

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -1,80 +1,46 @@
 import React from 'react';
-import { Box, Flex, Text, Heading, Center } from '@chakra-ui/react';
-import cat from '../../../public/Designer.jpeg'; // Assuming Designer.jpeg is your cat image
+import { Box, Flex, Text, Heading } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/useReduxHook';
+import cat from '../../../public/Designer.jpeg';
 
 export default function MainPage(): JSX.Element {
   const user = useAppSelector((state) => state.auth.user);
+
   return (
     <Flex justifyContent="center" alignItems="center" gap={4} marginTop="100px">
       <Box>
         <Box w="300px" h="300px" overflow="hidden" borderRadius="md">
-          <img src={cat} alt="Котик бариста" style={{ objectFit: 'cover' }} />
+          <img src={cat} alt="Котик бариста" style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
         </Box>
       </Box>
       <Box marginTop="10px" bg="#4F535E" borderRadius="md" p={4}>
-        <Heading fontSize="2xl" textColor="#FDA065">Бариста Кот: Ток Рандомич</Heading>
-      {user.status === 'logged' ?(<Box>
-
-
-        <div className="bubble grow left">
-          <Text className="p" fontSize="xl">
-            Здравствуй пирожочек!
-          </Text>
-          <Text className="p" fontSize="xl">
-            Рад тебя видеть
-          </Text>
-          <Text className="p" fontSize="xl">
-            Заходи присаживайся или
-          </Text>
-          <Text className="p" fontSize="xl">
-            ты хочешь поискать с кем поговорить?{' '}
-          </Text>
-        </div>
-        <Flex justifyContent="center" gap={4} mt={4}>
-          <div className="container">
-            <Link to='/addpoint'><button className="btn">Посидеть</button></Link>
-            
+        <Heading fontSize="2xl" color="#FDA065">Бариста Кот: Ток Рандомич</Heading>
+        <Box>
+          <div className="bubble grow left">
+            <Text className="p" fontSize="xl" color="black">
+              Здравствуй пирожочек!
+            </Text>
+            <Text className="p" fontSize="xl" color="black">
+              Рад тебя видеть
+            </Text>
+            <Text className="p" fontSize="xl" color="black">
+              Заходи присаживайся или
+            </Text>
+            <Text className="p" fontSize="xl" color="black">
+              ты хочешь поискать с кем поговорить?{' '}
+            </Text>
           </div>
-          <div className="container">
-          <Link to='/mappage'><button className="btn">Поискать</button></Link>
-            
-          </div>
-        </Flex>
-
-        
-      </Box>):
-      (<Box>
-        <Heading fontSize="2xl">Бариста Кот: Ток Рандомич</Heading>
-
-        <div className="bubble grow left">
-          <Text className="p" fontSize="xl">
-            Здравствуй пирожочек!
-          </Text>
-          <Text className="p" fontSize="xl">
-            Рад тебя видеть
-          </Text>
-          <Text className="p" fontSize="xl">
-            для начала давай
-          </Text>
-          <Text className="p" fontSize="xl">
-            зарегестрируйся или залогинься.{' '}
-          </Text>
-        </div>
-        <Flex justifyContent="center" gap={4} mt={4}>
-          <div className="container">
-            <Link to='/signin'><button className="btn">Логин</button></Link>
-            
-          </div>
-          <div className="container">
-          <Link to='/signup'><button className="btn">Регист-ия</button></Link>
-            
-          </div>
-        </Flex>
-
-        
-      </Box>)}
+          <Flex justifyContent="center" gap={4} mt={4}>
+            <div className="container">
+              <Link to='/addpoint'><button className="btn" style={{ color: 'white' }}>Посидеть</button></Link>
+            </div>
+            <div className="container">
+              <Link to='/mappage'><button className="btn" style={{ color: 'white' }}>Поискать</button></Link>
+            </div>
+          </Flex>
+        </Box>
+      </Box>
     </Flex>
   );
 }
--- client/src/components/pages/UserPage.tsx
+++ client/src/components/pages/UserPage.tsx
@@ -1,8 +1,8 @@
 import { Flex, Text, WrapItem, Box } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useAppSelector } from '../../hooks/useReduxHook'
 import PointCard from '../ui/PointCard'
-import { useDispatch } from 'react-redux'
 import { getPointsThunk } from '../../redux/thunkActions/mapThunkAction'
 import HistoryCard from '../ui/HistoryCard'
 import { fetchHistoryThunk } from '../../redux/thunkActions/historyThunk'
@@ -25,28 +25,28 @@ export default function UserPage({}: Props) {
 console.log(user.status === "logged" ? user.rating : "");
 
   return (
-    <div style={{ backgroundColor: 'FDA065', padding: '40px', borderRadius: '10px', color: '#fff', fontFamily: 'sans-serif' }}>
-      <div style={{ fontSize: '28px', marginBottom: '30px', textAlign: 'center', border: 'dashed 3px white', width: "max-content", margin: "auto"}}>Здравствуй, {user.status === 'logged' ? user.username : 'Guest'} !</div>
-      <WrapItem justifyContent="center">
-        <Text>Ваш рейтинг: {user.status === "logged" ? user.rating : ""} <i className="fa-solid fa-star"></i> </Text>
-      </WrapItem>
-      <Flex style={{ marginTop: '40px', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
-        <Box>
-        <Text fontSize="xl" mb="20px">Активные точки:</Text>
-        {points.map((point) =>
-          <PointCard point={point} key={point.id} user={user} />
-        )} <br />
-        </Box>
-        <Box>
-        <Text>История:</Text>
-        {histories.map((history) => {
-            const historyPoint = points.find((point) => point.id === history.pointId);
-            return (
-              <HistoryCard history={history} key={history.id} user={user} point={historyPoint}/>
-          );
-        })}
-        </Box>
-      </Flex>
-    </div>
+<div style={{ backgroundColor: '#4F535E', padding: '40px', borderRadius: '10px', color: '#fff', fontFamily: 'sans-serif', marginTop: "50px" }}>
+  <div style={{ fontSize: '28px', marginBottom: '30px', textAlign: 'center', border: 'dashed 3px white', width: "max-content", margin: "auto"}}>Здравствуй, {user.status === 'logged' ? user.username : 'Guest'} !</div>
+  <WrapItem justifyContent="center">
+    <Text>Ваш рейтинг: {user.status === "logged" ? user.rating : ""} <i className="fa-solid fa-star" /> </Text>
+  </WrapItem>
+  <Flex style={{ marginTop: '40px', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+    <Box>
+      <Text fontSize="xl" mb="20px">Активные точки:</Text>
+      {points.map((point) =>
+        <PointCard point={point} key={point.id} user={user} />
+      )} <br />
+    </Box>
+    <Box>
+      <Text>История:</Text>
+      {histories.map((history) => {
+        const historyPoint = points.find((point) => point.id === history.pointId);
+        return (
+          <HistoryCard history={history} key={history.id} user={user} point={historyPoint}/>
+        );
+      })}
+    </Box>
+  </Flex>
+</div>
   )
 }
